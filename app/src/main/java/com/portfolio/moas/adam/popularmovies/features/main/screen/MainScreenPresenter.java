c64c2b4cf490c9e1b740422962ad2f5ca6be5ec9
Adam Moas <adam.moas.1@gmail.com>
2018-07-24 21:41:18 -0700

    Creates Constructor

--- app/src/main/java/com/portfolio/moas/adam/popularmovies/features/main/screen/MainScreenPresenter.java
+++ app/src/main/java/com/portfolio/moas/adam/popularmovies/features/main/screen/MainScreenPresenter.java
@@ -1,6 +1,23 @@
 package com.portfolio.moas.adam.popularmovies.features.main.screen;
 
+import android.support.annotation.NonNull;
+
+import com.portfolio.moas.adam.popularmovies.data.remote.MovieDbService;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+
 public class MainScreenPresenter implements MainScreenContract.Presenter {
+
+    private MainScreenContract.View mainScreenView;
+    private MovieDbService movieDbService;
+
+    MainScreenPresenter(@NonNull MainScreenContract.View mainScreenView,
+                        @NonNull MovieDbService movieDbService) {
+        this.mainScreenView = checkNotNull(mainScreenView);
+        this.movieDbService = checkNotNull(movieDbService);
+    }
+
     @Override
     public void start() {
     }
