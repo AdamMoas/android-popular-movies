87f8e85a2e48a80ff0d9ead0e5b71158d9424abb
Adam Moas <adam.moas.1@gmail.com>
2018-08-01 21:36:53 -0700

    Adds Mockito dependency

--- app/src/test/java/com/portfolio/moas/adam/popularmovies/MainScreenPresenterTest.java
+++ app/src/test/java/com/portfolio/moas/adam/popularmovies/MainScreenPresenterTest.java
@@ -1,12 +1,13 @@
 package com.portfolio.moas.adam.popularmovies;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class MainScreenPresenterTest {
-
     @Test
     public void loadMovieData_ShouldSucceed() {
-        //TODO
+        //TODO WIP
+
     }
 
 
